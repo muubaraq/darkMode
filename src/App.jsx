8f87c2d88c67b535177d8ccdaac2dab6be465e37
mubaraq <sixthlogic@gmail.com>
2023-08-26 15:24:19 +0100

    darkMode

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,26 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [isDarkMode, setIsDarkMode] = useState(false);
+const toggleMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+     <div className={`App ${isDarkMode ? 'dark' : 'light'}`}>
+      <button className="mode-toggle" onClick={toggleMode}>
+        {isDarkMode ? 'Light Mode' : 'Dark Mode'}
+      </button>
+      <header>
+        <h1>{isDarkMode ? 'Dark Mode' : 'Light Mode'}</h1>
+      </header>
+      <main>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          This is a simple one-page portfolio website with dark and light mode.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        {/* Add your portfolio content here */}
+      </main>
+    </div>
     </>
   )
 }
